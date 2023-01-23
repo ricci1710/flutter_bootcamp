76e29d733fc3836825f706f053d3ae3caf71239b
Rico Kunz <rico.kunz@leutek.de>
2023-01-23 17:18:03 +0100

    I am rich lesson 2

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,20 @@ import 'package:flutter/material.dart';
 // The main function is the starting point for all our Flutter apps.
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8='),
+          ),
+        ),
       ),
     ),
   );
