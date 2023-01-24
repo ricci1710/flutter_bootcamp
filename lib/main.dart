ce8073eab0550c4fb950c4952d4225bc718a052a
Rico Kunz <ricci1710@gmail.com>
2023-01-24 09:05:16 +0100

    I am rich, lesson 2

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,7 @@ void main() {
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage(
-                'https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8='),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
