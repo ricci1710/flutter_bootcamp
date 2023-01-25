ed90c11cc9dd3745c58f4306020762978613d889
ricci1710 <ricci1710@gmail.com>
2023-01-25 17:32:47 +0100

    section 6: Use Container Widgets

--- lib/section7/micard.dart
+++ lib/section7/micard.dart
@@ -11,6 +11,10 @@ class MiCard extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: const EdgeInsets.only(left: 30.0),
+            padding: const EdgeInsets.all(20.0),
             color: Colors.white,
             child: const Text('Hello'),
           ),
